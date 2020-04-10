93b249bd5f77485e8e99737fe2d782f673a70d59
Dakar13 <ecalvamtz@gmail.com>
2020-04-10 16:01:11 -0500

    Operacion agregar curso creada en mutatios y funcionando

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -1,8 +1,27 @@
 import { IResolvers } from 'graphql-tools';
 import _ from 'lodash';
+import { databases } from '../data/data.store';
 
 const mutation : IResolvers = {
-    
+    Mutation: {
+        cursoNew(__:void, { curso }): any {
+            const itemCurso = {
+                id: String(databases.cursos.length + 1),
+                title: curso.title,
+                description: curso.description,
+                clases: curso.clases,
+                time: curso.time,
+                level: curso.level,
+                logo: curso.logo,
+                path: curso.path,
+                teacher: curso.teacher,
+                reviews: []
+            }
+
+            databases.cursos.push(itemCurso);
+            return itemCurso;
+        }
+    }
 }
 
 export default mutation;
