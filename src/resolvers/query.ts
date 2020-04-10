e07c448492aa91bbef6733abd5648265135c5660
Dakar13 <ecalvamtz@gmail.com>
2020-04-09 21:47:23 -0500

    Resolvers y Query para consultar un solo estudiante funcionando

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -5,6 +5,18 @@ const query : IResolvers = {
     Query: {
         estudiantes(): any {
             return databases.estudiantes;
+        },
+        estudiante(__: void, { id }): any {
+            const resultado = databases.estudiantes.filter(estudiante => estudiante.id === id) [0];
+            if (resultado === undefined) {
+                return {
+                    id: '-1',
+                    name: `No se ha encontrado el registro con el ID ${id}`,
+                    email: '',
+                    courses: []
+                }
+            }
+            return resultado;
         }
     }
 }
